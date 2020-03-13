1aa49eaf5843a03824a10aa66de0512280a164b4
Amin <amin.golmahalle@gmail.com>
2020-03-13 16:50:12 +0330

    Add Crud Apis Structure

--- Services/peopleService.py
+++ Services/peopleService.py
@@ -6,3 +6,23 @@ sub = Blueprint('hello_api', __name__, url_prefix='/api/people')
 @sub.route('/getAll')
 def get_all():
     return 'show get all people list'
+
+
+@sub.route('/getById/<int:id>')
+def get_by_id():
+    return 'show get all people list'
+
+
+@sub.route('/add', methods=['POST'])
+def add():
+    return 'show get all people list'
+
+
+@sub.route('/edit', methods=['PUT'])
+def edit():
+    return 'show get all people list'
+
+
+@sub.route('/delete/<int:id>', methods=['DELETE'])
+def delete_by_id():
+    return 'show get all people list'
