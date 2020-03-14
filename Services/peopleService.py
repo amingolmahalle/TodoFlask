1e83914117aa372058bae029d3cd2ca30bc34168
Amin <amin.golmahalle@gmail.com>
2020-03-14 18:22:07 +0330

    Refactor Code

--- Services/peopleService.py
+++ Services/peopleService.py
@@ -1,28 +1,70 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from models import db
 
 sub = Blueprint('hello_api', __name__, url_prefix='/api/people')
 
 
 @sub.route('/getAll')
 def get_all():
-    return 'show get all people list'
+    return db.query.all()
 
 
 @sub.route('/getById/<int:id>')
 def get_by_id():
-    return 'show get all people list'
+    return db.query.get(id)
 
 
 @sub.route('/add', methods=['POST'])
 def add():
-    return 'show get all people list'
+    request_data = request.json if request.is_json else request.form
+
+    fullname = request_data['fullname']
+    mobile = request_data['mobile']
+    birth_date = request_data['birth_date']
+    email = request_data['email']
+    status = request_data['status']
+
+    new_person = db(id, fullname, mobile, birth_date, email, status)
+
+    db.add(new_person)
+
+    try:
+        db.session.commit()
+    except:
+        db.session.rollback()
 
 
-@sub.route('/edit', methods=['PUT'])
-def edit():
-    return 'show get all people list'
+@sub.route('/edit/<int:id>', methods=['PUT'])
+def edit(id):
+    request_data = request.json if request.is_json else request.form
+
+    fullname = request_data['fullname']
+    mobile = request_data['mobile']
+    birth_date = request_data['birth_date']
+    email = request_data['email']
+    status = request_data['status']
+
+    person = db.query.get(id)
+
+    person.fullname = fullname
+    person.mobile = mobile
+    person.birth_date = birth_date
+    person.email = email
+    person.status = status
+
+    try:
+        db.session.commit()
+    except:
+        db.session.rollback()
 
 
 @sub.route('/delete/<int:id>', methods=['DELETE'])
-def delete_by_id():
-    return 'show get all people list'
+def delete_by_id(id):
+    person = db.query.get(id)
+
+    db.session.delete(person)
+
+    try:
+        db.session.commit()
+    except:
+        db.session.rollback()
--- models.py
+++ models.py
@@ -5,17 +5,18 @@ db = SQLAlchemy()
 
 class Person(db.Model):
     __tablename__ = "people"
-    id = db.Column(db.Integer, primary_key=True)
-    fullName = db.Column(db.String(30), nullable=False)
+    id = db.Column(db.Integer, db.Sequence('seq_reg_id', start=1, increment=1), primary_key=True)
+    fullName = db.Column(db.String(30), index=True, nullable=False)
     mobile = db.Column(db.String(11), unique=True, nullable=False)
     birthDate = db.Column(db.DateTime)
     email = db.Column(db.String(50), nullable=False)
     status = db.Column(db.Boolean, default=True)
     creationDate = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    def __init__(self, name, mobile, birth_date, email, status):
-        self.name = name
+    def __init__(self, id, fullname, mobile, birth_date, email, status):
+        self.id = id
+        self.fullname = fullname
         self.mobile = mobile
-        self.birthDate = birth_date
+        self.birth_date = birth_date
         self.email = email
         self.status = status
